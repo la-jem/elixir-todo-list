dd72f05d34926fcaf1914524d52141ef9e9b1510
la-jem <137056854+la-jem@users.noreply.github.com>
2023-07-03 07:07:07 +0800

    add edit todo functionality

--- lib/live/todo_live.ex
+++ lib/live/todo_live.ex
@@ -6,7 +6,7 @@ defmodule ElixirTodoListWeb.TodoLive do
   def mount(_params, _session, socket) do
     items = Item.list_items()
 
-    socket = assign(socket, items: items)
+    socket = assign(socket, items: items, editing: nil)
 
     {:ok, socket}
   end
@@ -16,6 +16,9 @@ defmodule ElixirTodoListWeb.TodoLive do
   def handle_event("save-item", %{"text" => text}, socket) do
     IO.inspect(blank?(text))
 
+    # Refactor to its own handler
+    # Check if text is blank
+    # Trigger appropriate action
     case blank?(text) do
       false ->
         Item.create_item(%{text: text})
@@ -45,6 +48,31 @@ defmodule ElixirTodoListWeb.TodoLive do
     |> (&{:noreply, &1}).()
   end
 
+  def handle_event("update-item", %{"id" => item_id, "text" => text}, socket) do
+    case blank?(text) do
+      false ->
+        current_item = Item.get_item!(item_id)
+        Item.update_item(current_item, %{text: text})
+
+        socket
+        |> clear_flash()
+        |> put_flash(:info, "Item updated successfully")
+        |> assign(items: Item.list_items(), editing: nil)
+        |> (&{:noreply, &1}).()
+
+      true ->
+        socket
+        |> clear_flash()
+        |> put_flash(:error, "Item text should not be empty")
+        |> assign(items: Item.list_items(), editing: nil)
+        |> (&{:noreply, &1}).()
+    end
+  end
+
+  def handle_event("update-item", data, socket) do
+    {:noreply, assign(socket, editing: String.to_integer(data["id"]))}
+  end
+
   def render(assigns) do
     ~H"""
     <div id="todo-list">
@@ -64,10 +92,21 @@ defmodule ElixirTodoListWeb.TodoLive do
       </form>
 
       <div :for={item <- @items} class="item">
-        <div class="text">
-          <%= item.text %>
-        </div>
-        <button class="delete" phx-click="delete-item" phx-value-id={item.id}>Delete Todo</button>
+        <%= if item.id == @editing do %>
+          <form phx-submit="update-item" id="form-update">
+            <input id="update-item" class="new-item" type="text" name="text" value={item.text} />
+            <input type="hidden" name="id" value={item.id} />
+            <button class="update" phx-disable-with="Saving...">
+              Update Todo
+            </button>
+          </form>
+        <% else %>
+          <div class="text">
+            <%= item.text %>
+          </div>
+          <button class="update" phx-click="update-item" phx-value-id={item.id}>Edit Todo</button>
+          <button class="delete" phx-click="delete-item" phx-value-id={item.id}>Delete Todo</button>
+        <% end %>
       </div>
     </div>
     """
